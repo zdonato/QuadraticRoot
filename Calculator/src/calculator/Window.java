14945a3e69dedcaa7330f0161f1d5bd7a22df331
Zachary Donato <zdonato@stevens.edu>
2014-03-11 20:49:58 -0400

    Fixed some mistakes and improved error checking.

--- Calculator/src/calculator/Window.java
+++ Calculator/src/calculator/Window.java
@@ -15,9 +15,10 @@ import java.awt.event.ActionListener;
 public class Window extends JFrame{
 	// Initialize data members. 
 	private static final long serialVersionUID = 1L;
-	private JLabel root1, root2, aLabel, bLabel, cLabel, title; 
+	private JLabel root1, root2, aLabel, bLabel, cLabel, title, error, imageLabel; 
 	private JButton submit;
 	private JTextField a, b, c; 
+	private ImageIcon image; 
 	private Calculator calc = new Calculator(); 
 	
 	// Set up the constructor for the window. 
@@ -26,15 +27,24 @@ public class Window extends JFrame{
 		setLayout(new GridBagLayout());
 		GridBagConstraints g = new GridBagConstraints();
 		Event e = new Event(); 
+		image = new ImageIcon(getClass().getResource("quad.jpg")); 
+		getContentPane().setBackground(Color.LIGHT_GRAY);
+		
+		imageLabel = new JLabel(image); 
+		g.fill = GridBagConstraints.HORIZONTAL;
+		g.gridx = 0;
+		g.gridy = 0;
+		add(imageLabel, g); 
 		
 		title = new JLabel("Calculate Quadratic Root"); 
-		title.setForeground(Color.BLUE);
+		title.setForeground(Color.WHITE);
 		g.fill = GridBagConstraints.HORIZONTAL;
 		g.gridx = 1;
 		g.gridy = 0;
-		add(title, g); 
+		add(title, g);
 		
-		aLabel = new JLabel("A:"); 
+		aLabel = new JLabel("A:");
+		aLabel.setForeground(Color.WHITE);
 		g.fill = GridBagConstraints.HORIZONTAL; 
 		g.gridx = 0; 
 		g.gridy = 1; 
@@ -47,6 +57,7 @@ public class Window extends JFrame{
 		add(a, g); 
 		
 		bLabel = new JLabel("B:");
+		bLabel.setForeground(Color.WHITE);
 		g.fill = GridBagConstraints.HORIZONTAL; 
 		g.gridx = 0; 
 		g.gridy = 2; 
@@ -58,7 +69,8 @@ public class Window extends JFrame{
 		g.gridy = 2;
 		add(b, g); 
 		
-		cLabel = new JLabel("C:"); 
+		cLabel = new JLabel("C:");
+		cLabel.setForeground(Color.WHITE);
 		g.fill = GridBagConstraints.HORIZONTAL; 
 		g.gridx = 0; 
 		g.gridy = 3;
@@ -70,7 +82,7 @@ public class Window extends JFrame{
 		g.gridy = 3; 
 		add(c, g); 
 		
-		submit = new JButton("Submit"); 
+		submit = new JButton("Calculate"); 
 		g.fill = GridBagConstraints.HORIZONTAL; 
 		g.gridx = 1; 
 		g.gridy = 4;
@@ -88,6 +100,12 @@ public class Window extends JFrame{
 		g.gridy = 2; 
 		add(root2, g); 
 		
+		// Add a third label in the 2nd row in case there is an error on c.
+		error = new JLabel(""); 
+		g.fill = GridBagConstraints.HORIZONTAL;
+		g.gridx = 2; 
+		g.gridy = 3;
+		add(error, g); 
 		
 		// Create an event to listen for submit click. 
 		submit.addActionListener(e); 
@@ -105,19 +123,16 @@ public class Window extends JFrame{
 			root1.setForeground(Color.BLACK);
 			root2.setText(""); 
 			root2.setForeground(Color.BLACK);
+			error.setText("");
+			error.setForeground(Color.BLACK);
 			
 			try {
 				// Calculate root1.
 				r1 = calc.getRoot1(Double.parseDouble(a.getText()), Double.parseDouble(b.getText()), Double.parseDouble(c.getText())); 
 				// Calculate root2.
 				r2 = calc.getRoot2(Double.parseDouble(a.getText()), Double.parseDouble(b.getText()), Double.parseDouble(c.getText())); 
-			} catch (Exception e1){
-				root1.setText("Enter a valid number."); 
-				root1.setForeground(Color.RED);
-			}
-			
-			try { 
-				// If the roots exist, check for equality.
+				
+				// Check for equality between roots for displaying.
 				if (r1.equals("none") || r2.equals("none"))
 					root1.setText("No real roots."); 
 				else if (r1.equals(r2))
@@ -126,8 +141,29 @@ public class Window extends JFrame{
 					root1.setText("Root 1: " + r1);
 					root2.setText("Root 2: " + r2);
 				}
-			} catch (NullPointerException n){
-				root1.setText("Enter a valid number.");
+				
+			} catch (NumberFormatException nfe){
+				// Try to parse each textField as a number and determine where the error is.
+				try {
+					Double.parseDouble(a.getText()); 
+				} catch (NumberFormatException ea){
+					root1.setForeground(Color.RED);
+					root1.setText("Enter a valid number."); 
+				}
+				
+				try {
+					Double.parseDouble(b.getText());
+				} catch (NumberFormatException eb){
+					root2.setForeground(Color.RED);
+					root2.setText("Enter a valid number."); 
+				}
+				
+				try {
+					Double.parseDouble(c.getText());
+				} catch (NumberFormatException ec){
+					error.setForeground(Color.RED);
+					error.setText("Enter a valid number."); 
+				}
 			}
 		} // End actionPerformed.
 	} // End Event class.
@@ -137,7 +173,7 @@ public class Window extends JFrame{
 		Window window = new Window(); 
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
-		window.setSize(500, 200); 
+		window.setSize(450, 270); 
 		window.setTitle("Quadratic Root Finder"); 
 	}
 	
