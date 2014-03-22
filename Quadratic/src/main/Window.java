cbda92a817e591d0491fca5dd2af15912709197e
Zachary Donato <zdonato@stevens.edu>
2014-03-22 12:38:32 -0400

    Updates.

--- Quadratic/src/main/Window.java
+++ Quadratic/src/main/Window.java
@@ -1,8 +1,7 @@
 package main; 
 
 import javax.swing.*; 
-
-import java.awt.*; 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
@@ -63,7 +62,7 @@ public class Window extends JFrame{
 			}
 
 			// Must implement focusLost.
-			public void focusLost(FocusEvent arg0) {
+			public void focusLost(FocusEvent arg) {
 				
 			}
 		});
